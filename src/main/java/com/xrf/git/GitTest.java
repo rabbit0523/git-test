98b7c5b24de4155edbf5e0419b8f397d833890af
xue <602120418@qq.com>
2024-03-16 12:25:52 +0800

    first commit

---
+++ src/main/java/com/xrf/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xrf.git;
+
+/**
+ * @author xueruifeng
+ * @create 2024-03-16 12:21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}

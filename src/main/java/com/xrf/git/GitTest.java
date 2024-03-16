e17e3af52cfbd9b3f2f047e0f5b5a60406dd3d5d
rabbit0523 <69590630+rabbit0523@users.noreply.github.com>
2024-03-16 17:45:24 +0800

    pull test

--- src/main/java/com/xrf/git/GitTest.java
+++ src/main/java/com/xrf/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("second commit");
+        System.out.println("pull test");
     }
 }
